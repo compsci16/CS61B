1bd8a597418f1768c8999126c7996c73aab02e24
wint <wintcred@gmail.com>
2021-08-12 16:02:55 +0530

    Finished proj 2 phase 1

--- proj2/byog/Core/WorldVisualTest.java
+++ proj2/byog/Core/WorldVisualTest.java
@@ -2,18 +2,21 @@ package byog.Core;
 
 import byog.TileEngine.TETile;
 
+import java.util.Random;
+
 public class WorldVisualTest {
     private static final int WIDTH = 100;
     private static final int HEIGHT = 50;
 
     public static void main(String[] args) {
+        final long NANOSEC_PER_SEC = 1000l*1000*1000;
         TETile[][] tiles = new TETile[WIDTH][HEIGHT];
         World w = new World(6441, tiles, WIDTH, HEIGHT);
-        w.initialize();
-//        for (int i = 1; i < 100000; i++) {
-//            w = new World(i, tiles, WIDTH, HEIGHT);
-//            w.initialize();
-//        }
+        long startTime = System.nanoTime();
+        while ((System.nanoTime()-startTime)< 1*60*NANOSEC_PER_SEC){
+            w = new World(System.nanoTime(), tiles, WIDTH, HEIGHT);
+            w.initialize();
+        }
         w.render();
     }
 }
