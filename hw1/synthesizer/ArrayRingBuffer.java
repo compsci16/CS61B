9d1c9a62ec376c341b94080ab113ce337fac8439
wint <wintcred@gmail.com>
2021-08-18 22:20:48 +0530

    finished hw1

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,89 @@
+package synthesizer;
+
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    private int first, last;
+    private T arr[];
+
+    public ArrayRingBuffer(int capacity) {
+        first = -1;
+        last = 0;
+        this.capacity = capacity;
+        arr = (T[]) new Object[capacity];
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder q = new StringBuilder();
+        for (int i = first; i < capacity; i++) {
+            if (arr[i] == null)
+                break;
+            q.append(arr[i]).append(" ");
+        }
+        for (int i = 0; i < first; i++) {
+            if (arr[i] == null)
+                break;
+            q.append(arr[i]).append(" ");
+        }
+        return q.toString().trim();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new BufferIterator();
+    }
+
+    private class BufferIterator implements Iterator<T> {
+        private int ptr;
+        private int iterateThrough;
+
+        public BufferIterator() {
+            ptr = first;
+            iterateThrough = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterateThrough != fillCount;
+        }
+
+        @Override
+        public T next() {
+            T returnItem = arr[ptr];
+            ptr = limitTheIndex(++ptr);
+            ++iterateThrough;
+            return returnItem;
+        }
+    }
+
+    @Override
+    public void enqueue(T x) {
+        if (isFull())
+            throw new RuntimeException("Ring  Buffer Overflow");
+        if (isEmpty())
+            first = last;
+        fillCount++;
+        arr[last] = x;
+        last = limitTheIndex(++last);
+    }
+
+    @Override
+    public T dequeue() {
+        if (isEmpty())
+            throw new RuntimeException("Ring Buffer Underflow");
+        fillCount--;
+        T toRemove = arr[first];
+        first = limitTheIndex(++first);
+        return toRemove;
+    }
+
+    private int limitTheIndex(int index) {
+        return index % capacity;
+    }
+
+    @Override
+    public T peek() {
+        return arr[first];
+    }
+}
--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,18 +1,77 @@
 package synthesizer;
+
+import edu.princeton.cs.algs4.In;
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-/** Tests the ArrayRingBuffer class.
- *  @author Josh Hug
+/**
+ * Tests the ArrayRingBuffer class.
+ *
+ * @author Josh Hug
  */
 
 public class TestArrayRingBuffer {
-    @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    private static ArrayRingBuffer<Integer> arb;
+
+    @Before
+    public void init() {
+        arb = new ArrayRingBuffer<>(4);
     }
 
-    /** Calls tests for ArrayRingBuffer. */
+    @Test
+    public void testEnqueue() {
+        arb.enqueue(0);
+        assertEquals("0", arb.toString());
+        arb.enqueue(1);
+        assertEquals("0 1", arb.toString());
+        arb.enqueue(2);
+        arb.enqueue(3);
+        assertEquals("0 1 2 3", arb.toString());
+
+        // no enqueue after full:
+        arb.enqueue(3);
+        arb.enqueue(3);
+        assertEquals("0 1 2 3", arb.toString());
+    }
+
+    private static Integer Int(int x) {
+        return x;
+    }
+
+    @Test
+    public void testDequeue() {
+        arb.enqueue(0);
+        assertEquals(Integer.valueOf(0), arb.dequeue());
+        arb.enqueue(0);
+        arb.enqueue(1);
+        assertEquals(Int(0), arb.dequeue());
+        assertEquals(Int(1), arb.peek());
+
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+
+        assertEquals(Int(1), arb.peek());
+        assertEquals(4, arb.fillCount());
+        assertEquals(Int(1), arb.dequeue());
+    }
+
+    @Test
+    public void testForEach() {
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+        for (Integer i : arb) {
+            System.out.print(i); // expect 1 to 4
+        }
+    }
+
+    /**
+     * Calls tests for ArrayRingBuffer.
+     */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
     }
