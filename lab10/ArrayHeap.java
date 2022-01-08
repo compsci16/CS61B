33a20c7a82851a028c75efff32772b3ea555b81d
wint <wintcred@gmail.com>
2022-01-09 02:31:08 +0530

    HashMap done

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -1,6 +1,6 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * A Generic heap class. Unlike Java's priority queue, this heap doesn't just
